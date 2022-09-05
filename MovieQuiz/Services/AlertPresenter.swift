1b2817aac9e9c104fb30708f7cb8b7f28cd102bd
Nika Masimova <masimostudy#gmail.com>
2022-09-05 20:27:42 +0300

    add error alert

--- MovieQuiz/Services/AlertPresenter.swift
+++ MovieQuiz/Services/AlertPresenter.swift
@@ -1,7 +1,7 @@
 import Foundation
 import UIKit
 
-class ResultAlertPresenter {
+class AlertPresenter {
     func showResults(quiz step: QuizResultsViewModel, alertPresenter: UIViewController, onAction: @escaping (UIAlertAction) -> Void) {
         let alert = UIAlertController(
             title: step.title,
@@ -9,6 +9,13 @@ class ResultAlertPresenter {
             preferredStyle: .alert
         )
         let action = UIAlertAction(title: step.buttonText, style: .default, handler: onAction)
+        alert.addAction(action)
+        alertPresenter.present(alert, animated: true, completion: nil)
+    }
+
+    func showError(message: String, title: String, buttonTitle: String, alertPresenter: UIViewController, onAction: @escaping (UIAlertAction) -> Void) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: buttonTitle, style: .default, handler: nil)
         alert.addAction(action)
         alertPresenter.present(alert, animated: true, completion: nil)
     }
